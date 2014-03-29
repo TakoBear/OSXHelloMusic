387298808bedd1866a9d9e16f2b55351d3752273
Bear <timfanda35@gmail.com>
2014-03-29 15:20:55 +0800

    v0.1.0

---
+++ HelloMusic/MainWindowController.h
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.h
+//  HelloMusic
+//
+//  Created by Bear on 2014/3/29.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainWindowController : NSWindowController
+
+@property (weak) IBOutlet NSTextField *infoTextField;
+
+@end
--- HelloMusic/bearAppDelegate.h
+++ HelloMusic/bearAppDelegate.h
@@ -8,8 +8,12 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class MainWindowController;
+
 @interface bearAppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, strong) MainWindowController *mainWindowController;
 
 @end
+
+
